9a40187150842168f6b06774e87d176334e7424b
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2024-02-05 15:13:22 +0300

    Update documentation

---
+++ assets/readme_index.md.JrQ2sdiY.lean.js
@@ -0,0 +1,1 @@
+import{_ as a,c as s,o as i,V as n}from"./chunks/framework.wFS8Ix-U.js";const g=JSON.parse('{"title":"About","description":"","frontmatter":{},"headers":[],"relativePath":"readme/index.md","filePath":"readme/index.md"}'),e={name:"readme/index.md"},l=n("",12),t=[l];function p(o,h,r,c,d,k){return i(),s("div",null,t)}const u=a(e,[["render",p]]);export{g as __pageData,u as default};
