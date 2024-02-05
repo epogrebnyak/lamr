06ca6eb8237d3af5b7e076015228cb8ad399a42d
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2024-02-05 15:20:44 +0300

    Update documentation

---
+++ assets/readme_index.md.QLeRGtMS.lean.js
@@ -0,0 +1,1 @@
+import{_ as a,c as s,o as i,V as n}from"./chunks/framework.OhuHbfjs.js";const g=JSON.parse('{"title":"About","description":"","frontmatter":{},"headers":[],"relativePath":"readme/index.md","filePath":"readme/index.md"}'),e={name:"readme/index.md"},t=n("",12),l=[t];function p(o,h,r,c,d,k){return i(),s("div",null,l)}const u=a(e,[["render",p]]);export{g as __pageData,u as default};
