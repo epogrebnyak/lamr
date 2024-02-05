db71cec3dd6fb68629e6b6e63a68d63c31f25453
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2024-02-05 18:35:00 +0300

    Update documentation

---
+++ assets/code_logo.py.md.ktkawgbb.lean.js
@@ -0,0 +1,1 @@
+import{_ as i,c as s,o as a,V as t}from"./chunks/framework.OhuHbfjs.js";const E=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"code/logo.py.md","filePath":"code/logo.py.md"}'),e={name:"code/logo.py.md"},n=t("",9),l=[n];function h(o,r,p,k,c,d){return a(),s("div",null,l)}const y=i(e,[["render",h]]);export{E as __pageData,y as default};
